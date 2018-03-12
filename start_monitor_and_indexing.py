7a00285eb562e8e64ce207852af23ef86cb03b5d
emilianobaum <emilianobaum@gmail.com>
2018-03-12 16:17:51 +0000

    Changes in index conf.

--- start_monitor_and_indexing.py
+++ start_monitor_and_indexing.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 
-from monitor_unit import StartService as SS
+from monitor_unit import StartService as MU
 from es_index__module import ESIndices
 from data_server import DataServer
 from load_config import ConfigData
@@ -20,12 +20,10 @@ logger = logging.getLogger("Monitor & Indexing Unit")
 class MonitorModule(ConfigData):
     
     def load_logger(self, data):
-        #=======================================================================
-        # Load logger configuration.
-        #=======================================================================
-        logger = logging.getLogger("Monitor Unit")
-        #~ logger.setLevel(logging.DEBUG)
-        logger.setLevel(logging.INFO)
+        """
+        Load logger configuration.
+        """
+        logger.setLevel(logging.DEBUG)
         handler = logging.handlers.RotatingFileHandler(
             filename=('%s/%s'%(data.dirLog, data.fileLog)),
                       mode='a', maxBytes=5000000, backupCount=5, 
@@ -38,9 +36,17 @@ class MonitorModule(ConfigData):
         return True
 
     def __init__(self):
+        __author__ = "Emiliano A. Baum"
+        __contact__ = "ebaum@conae.gov.ar"
+        __copyrigth__ = "2018-01-26, Monitoring unit module."
+        __license__ = "GPLv3"
+        __version__ = ("20180202, v3.0 Using docker running like service")
+        __description__ = "Tool for monitor and indexing data to cluster and server for "
+        __last_revision__= "2018-03-12"
         
         DATA = CD("configurations/configuration.json")
         self.load_logger(DATA)
+        logger.info("Starting unit %s."%DATA.unitName)
         if DATA.elasticIndexTime == 'True':
             timeFormating = datetime.strftime(datetime.utcnow(), 
                                               DATA.elasticIndexTimeFormat)
@@ -57,25 +63,25 @@ class MonitorModule(ConfigData):
                        )
         proc.daemon = True
         proc.start()
-        logger.info("The process %s was created" % (Process.__name__))
+        logger.info("The process %s was created." % (Process.__name__))
         while True:
-            telemetry = SS(DATA).telemetry
-            print("Telemetry: ",telemetry)
-            if unitTelemetry.full() :
-                unitTelemetry.get()
-            unitTelemetry.put(telemetry)
-
-            if ESI.is_index_exist(DATA.elasticIndex) == False:
-                ESI.create_index(DATA.elasticIndex, 
-                                 DATA.elasticShards,
-                                 DATA.elasticReplicas
-                                 )
-            logger.debug("Connection successfully")
-            logger.debug("Formatting data ...")
-            indexPath ="%s%s"%(DATA.indexConf, DATA.elasticFile)
-
-            ESI.indexing_data(DATA.elasticIndex, DATA.elasticIndexType, 
-                              telemetry, DATA.elasticDefinition, indexPath)
+            telemetry = MU(DATA).telemetry
+            if telemetry:
+                if unitTelemetry.full() :
+                    unitTelemetry.get()
+                unitTelemetry.put(telemetry)
+    
+                if ESI.is_index_exist(DATA.elasticIndex) == False:
+                    ESI.create_index(DATA.elasticIndex, 
+                                     DATA.elasticShards,
+                                     DATA.elasticReplicas
+                                     )
+                logger.debug("Connection successfully.")
+                logger.debug("Formatting data ...")
+                indexPath ="%s%s"%(DATA.indexConf, DATA.elasticFile)
+    
+                ESI.indexing_data(DATA.elasticIndex, DATA.elasticIndexType, 
+                                  telemetry, DATA.elasticDefinition, indexPath)
             sleep(5)
 
 if __name__ == '__main__':
